77e2910f93dbbdc3dfe600a89e58c6e2477ab665
Parth Pathak <myselfparth26@gmail.com>
2019-11-24 13:40:15 +0530

    24-11-19 improved weight function, fast sweeping, distance map completed

---
+++ distance2.py
@@ -0,0 +1,56 @@
+import sys
+import math
+import numpy as np
+
+U = np.array([])
+
+def update(W, size, i, j):
+    h = 1
+    global U
+    if i==0:
+        UH = U[i+1][j]
+    elif i==size[0]-1:
+        UH = U[i-1][j]
+    else:
+        UH = min(U[i-1][j], U[i+1][j])
+    if j==0:
+        UV = U[i][j+1]
+    elif j==size[1]-1:
+        UV = U[i][j-1]
+    else:
+        UV = min(U[i][j-1], U[i][j+1])
+
+    if abs(UH - UV)<(h/W[i][j]):
+        val1 = math.pow(UH,2)+math.pow(UV,2)-math.pow(h/W[i][j],2)
+        val2 = (2*math.pow(h/W[i][j], 2))-math.pow(UH-UV, 2)
+        u = ((UH+UV)/2)+(0.5*math.sqrt(val2))
+    else:
+        u = min(UH,UV)+(h/W[i][j])
+    return u
+
+def fastsweeping(W, size, s1, s2):
+    global U
+    label = np.zeros(size)
+    U = np.ones(size)*sys.maxsize
+
+    U[s1[0]][s1[1]] = 0
+    label[s1[0]][s1[1]] = 1
+    
+    for i in range(size[0]):
+        for j in range(size[1]):
+            if label[i][j]==0:
+                U[i][j] = min(U[i][j], update(W, size, i, j))
+    for i in range(size[0]):
+        for j in range(size[1]-1, -1, -1):
+            if label[i][j]==0:
+                U[i][j] = min(U[i][j], update(W, size, i, j))
+    for i in range(size[0]-1, -1, -1):
+        for j in range(size[1]-1, -1, -1):
+            if label[i][j]==0:
+                U[i][j] = min(U[i][j], update(W, size, i, j))
+    for i in range(size[0]-1, -1, -1):
+        for j in range(size[1]):
+            if label[i][j]==0:
+                U[i][j] = min(U[i][j], update(W, size, i, j))
+    
+    return U
--- main.py
+++ main.py
@@ -10,20 +10,18 @@ from flow import gradientFlow
 from time import time
 import geodesic
 from distance import fastmarching
+from distance2 import fastsweeping
 import sys
 #import fast_sweeping
 
-print(sys.getrecursionlimit())
 t0 = time()
-input_img = cv2.imread('Dataset\\AMD_patient2\\90C683A0.tif', 0)
+input_img = cv2.imread('Dataset\\healthy_subject1\\61E9F5B0.tif', 0)
 t1 = time()
 print('Imread:  '+str(t1-t0)+' s')
 crop_img = cropImage(input_img)
 size = crop_img.shape
-#crop_img = cv2.resize(crop_img, (size[1]//8, size[0]//8), interpolation = cv2.INTER_AREA)
+crop_img = cv2.resize(crop_img, (size[1]//4, size[0]//4), interpolation = cv2.INTER_AREA)
 size = crop_img.shape
-sys.setrecursionlimit(max(1000, size[0]*size[1] + 1))
-print(sys.getrecursionlimit())
 t2 = time()
 print('Crop:  '+str(t2-t1)+' s')
 nl_img = cv2.fastNlMeansDenoising(crop_img,None,10,7,21)
@@ -31,8 +29,8 @@ t3 = time()
 print('NL-Means: '+str(t3-t2)+' s')
 thresh = LAT(nl_img)
 t4 = time()
-print('thresh: '+str(t4-t3)+' s')
-wt = weight.dark2bright(thresh)
+print('LAT: '+str(t4-t3)+' s')
+wt = weight.bright2dark(thresh)
 #idx = detectEdge(thresh)
 #wt = createEdge(crop_img, thresh, idx)
 t5 = time()
@@ -43,25 +41,30 @@ for i in range(size[0]):
         W[i][j] = wt[i][j-1]
 s1 = [0, 0]
 s2 = [size[0]-1, size[1]+1]
-D = fastmarching(W, (size[0], size[1]+2), s1, s2)
+t6 = time()
+print('Padding: '+str(t6-t5)+' s')
+D1 = fastmarching(W, (size[0], size[1]+2), s1, s2)
 #d = fast_sweeping.signed_distance(np.array(wt), 1.)
 #res = cv2.inpaint(thresh,wt,1,cv2.INPAINT_TELEA)
 #res = geodesic.detectBoundary(wt)
 #D = res[1]
-t6 = time()
-print('Distance Map: '+str(t6-t5)+' s')
+t7 = time()
+print('Fast Marching: '+str(t7-t6)+' s')
+D2 = fastsweeping(W, (size[0], size[1]+2), s1, s2)
 #arr = gradientFlow(D)
 #print(arr)
 #img = color.color(arr, crop_img)
-t7 = time()
+t8 = time()
 
-print('Gradient Descent: '+str(t7-t6)+' s')
-print('Total: '+str(t7-t0)+' s')
+print('Fast sweeping: '+str(t8-t7)+' s')
+print('Total: '+str(t8-t0)+' s')
 print()
 
-plt.subplot(2,2,1),plt.imshow(nl_img, cmap='gray'),plt.title('Crop & Denoised'),plt.xticks([])
-plt.subplot(2,2,2),plt.imshow(thresh, cmap='gray'),plt.title('LAT'),plt.xticks([])
-plt.subplot(2,2,3),plt.imshow(wt, cmap='gray'),plt.title('Weight'),plt.xticks([])
-plt.subplot(2,2,4),plt.imshow(D, cmap='gray'),plt.title('Geodesic'),plt.xticks([])
+plt.subplot(3,2,1),plt.imshow(crop_img, cmap='gray'),plt.title('Crop and Resize'),plt.xticks([])
+plt.subplot(3,2,2),plt.imshow(nl_img, cmap='gray'),plt.title('Denoised'),plt.xticks([])
+plt.subplot(3,2,3),plt.imshow(thresh, cmap='gray'),plt.title('LAT'),plt.xticks([])
+plt.subplot(3,2,4),plt.imshow(wt, cmap='gray'),plt.title('Weighted'),plt.xticks([])
+plt.subplot(3,2,5),plt.imshow(D1, cmap='gray'),plt.title('Fast Marching'),plt.xticks([])
+plt.subplot(3,2,6),plt.imshow(D2, cmap='gray'),plt.title('Fast Sweeping'),plt.xticks([])
 
 plt.show()
